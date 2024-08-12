544b17a101c7ab4986c39ab2b326cea8d5864eac
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-08-11 19:13:05 -0500

    Subiendo botones

--- Frontend/src/pages/AdministrarProducto.jsx
+++ Frontend/src/pages/AdministrarProducto.jsx
@@ -20,35 +20,30 @@ const AdministrarProducto = () => {
           <ul className="mt-5 flex flex-col space-y-4">
             <li className="text-center">
               <div className="text-white bg-blue-800 px-3 py-2 rounded-md text-xl hover:bg-blue-700">
-                <Link to="/dashboard">Inicio</Link>
-              </div>
-            </li>
-            <li className="text-center">
-              <div className="text-white bg-blue-800 px-3 py-2 rounded-md text-xl hover:bg-blue-700">
-                <Link to="/dashboard/listartienda">Buscar Tienda</Link>
+                <Link to="/dashboard/administrartienda">Volver</Link>
               </div>
             </li>
           </ul>
         </div>
         <div className="flex-grow flex justify-center items-center">
           <div className="bg-gray-900 bg-opacity-90 p-10 rounded-lg shadow-lg w-11/12 flex flex-col justify-start space-y-4 border border-gray-700 mt-10 mb-10 ml-10 mr-5">
-            <h1 className="font-black text-4xl text-gray-200">Buscar producto</h1>
+            <h1 className="font-black text-4xl text-gray-200">Actualizar Producto</h1>
             <hr className="my-4" />
             <p className="text-gray-200 mb-4">
-              Dentro de aquí podrás buscar el producto de tu interés a comprar su tienda y su ubicación
+              Dentro de aquí podrás modificar el estado de tu productos asi como su categoria y nombre
             </p>
             <div className="flex justify-between">
               <button
                 onClick={() => handleShowTable('nombreTienda')}
                 className="bg-green-500 px-3 py-2 text-slate-300 uppercase font-bold rounded-lg hover:bg-gray-900 cursor-pointer transition-all"
               >
-                Buscar Productos por Nombre de tienda
+                Actualizar el Estado de producto
               </button>
               <button
                 onClick={() => handleShowTable('categoria')}
                 className="bg-blue-500 px-3 py-2 text-slate-300 uppercase font-bold rounded-lg hover:bg-gray-900 cursor-pointer transition-all"
               >
-                Buscar Productos por categoría
+                Actualizar nombre y categoria del producto
               </button>
             </div>
             {mostrarTabla === 'categoria' && <ListarProductosPorCategoria />}
