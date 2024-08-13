cbd2c73777131f48c9d3a580eb2a17103dc160e4
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-08-13 09:54:44 -0500

    Pagina no encontrada tests

---
+++ Frontend/src/test/unitarias/NoEncontrada.test.js
@@ -0,0 +1,39 @@
+// __tests__/NoEncontrada.test.js
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { NoEncontrada } from '../../pages/NoEncontrada';
+
+describe('NoEncontrada Component', () => {
+  test('renders the NoEncontrada component with correct content, ignoring images', () => {
+    render(
+      <MemoryRouter>
+        <NoEncontrada />
+      </MemoryRouter>
+    );
+
+    // Verifica que el texto "¿Te Perdiste?" está presente
+    expect(screen.getByText('¿Te Perdiste?')).toBeInTheDocument();
+
+    // Verifica que el mensaje de error está presente
+    expect(screen.getByText('Lo sentimos, la página que intentas buscar no se encuentra disponible o no existe.')).toBeInTheDocument();
+
+    // Verifica que el enlace "ingresar" está presente y tiene el href correcto
+    const link = screen.getByText('ingresar');
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', '/ingresar');
+  });
+
+  test('renders the component with correct styles', () => {
+    render(
+      <MemoryRouter>
+        <NoEncontrada />
+      </MemoryRouter>
+    );
+
+    // Selecciona el contenedor más externo (el que tiene el fondo)
+    const container = screen.getByTestId('container');  // Usamos data-testid para un acceso más directo
+    expect(container).toHaveClass('min-h-screen w-full flex justify-center items-center bg-[url(\'/public/images/olvidarcontra.jpg\')] bg-no-repeat bg-cover bg-center');
+  });
+});
